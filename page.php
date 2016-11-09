f00c4962bb2b43df6fe884090f2c9d178939947c
Mike Langone <langone@onlineimage.com>
2016-11-09 13:42:04 -0700

    added files

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 	
 	<div id="content">
-	
+	<?php get_template_part( 'parts/featured-image' ); ?>	
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-8 medium-8 columns" role="main">
--- template-full-width.php
+++ template-full-width.php
@@ -7,7 +7,7 @@ Template Name: Full Width (No Sidebar)
 <?php get_header(); ?>
 			
 	<div id="content">
-	
+	<?php get_template_part( 'parts/featured-image' ); ?>
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-12 medium-12 columns" role="main">
