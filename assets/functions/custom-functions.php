5a82b578ce57927de553c18ea9cfe6cf69515306
Mike Langone <langone@onlineimage.com>
2016-11-09 15:36:14 -0700

    Added the ability to switch out a typekit id

--- assets/functions/custom-functions.php
+++ assets/functions/custom-functions.php
@@ -15,3 +15,19 @@ $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'them
 ) ) );
 }
 add_action('customize_register', 'themeslug_theme_customizer');
+
+//* Enqueue script to add typekit fonts if you dont use it then comment it our or just add a remove action 
+add_action('wp_enqueue_scripts', 'load_typkit_up');
+function load_typkit_up() { ?>
+	<script>
+	  (function(d) {
+		var config = {
+	// Change the kit id to yours
+		  kitId: 'tfj8xmd',
+		  scriptTimeout: 500,
+		  async: true
+		},
+		h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+	  })(document);
+	</script>
+<?php }
